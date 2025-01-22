9cc1a8a8180b8afcb3948ea10349b5ebccd0757e
JaoLF <jaumluiiiz@gmail.com>
2025-01-22 06:38:14 -0300

    projeto finalizado

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+function copyToClickBoard(){
+    let content = document.getElementById('dado').innerHTML;
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        alert("Email copiado!");
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+ 
+}
+
